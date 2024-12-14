587d92e03bb5a1363442358e4deb143c6b98f5aa
William <mrwilliamgregorio@yahoo.com>
2024-12-14 03:07:52 -0500

    eval on set instro for me

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+/* main */
+
+// have sockets -> x/net
+// have icmp help -> x/net
+// will need some time support so whichever eq to time.h from sys
+// create the command argv -> os
 func main() {
 
 }
