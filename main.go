97a9aec62ecbbb982fa04f0d27d9d2faa0adcd0a
William <mrwilliamgregorio@yahoo.com>
2024-12-14 03:13:26 -0500

    arg support discovery

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 /* main */
 
 // have sockets -> x/net
@@ -7,5 +12,7 @@ package main
 // will need some time support so whichever eq to time.h from sys
 // create the command argv -> os
 func main() {
-
+	for i, arg := range os.Args {
+		fmt.Printf("os.args[%d] -> %s\n", i, arg)
+	}
 }
