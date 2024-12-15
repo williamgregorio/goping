b6c675de4f25ba85b1ea1c12548f84debe943ca4
William <mrwilliamgregorio@yahoo.com>
2024-12-15 15:31:17 -0500

    aded maps,and rtt set layout

--- main.go
+++ main.go
@@ -13,11 +13,18 @@ import (
 // raw sockets -> interaction with icmp (send/recieve)!no=tcp/udp
 /* raw sockets el priv, for interaction on network stack -> maybe*/
 // log on measure of rtt on travel to (destination -> back to you)
+/*
+  rtt = time_reply_recieved - time_request_sent
+*/
 // packet loss if no response on (recieve) withing _timeout_ hence=loss
-/* packe loss log on tracking to diagnose network realibity*/
+/* packet loss log on tracking to diagnose network realibity*/
 /*
 intermittent network, focus on later after the concepts are ready
 */
+
+// tracerouting - ttl(increment ttl field in ip (packet(s)))
+// the idea is a router along a path decrements ttl, on 0, router sends us back and icmp time exceeded msg hence maps the path from response
+// destination reach => ^stops when target host responds with (icmp echo/reply)
 
 /* main */
 func main() {
